287bfc6c56f4793e375a9d94c3328574c7434c1f
Guloba <sesomape@gmail.com>
2021-08-11 18:11:46 +0300

    First commit

---
+++ ibring/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {apiKey: "AIzaSyD6lgC3EQNLzFtKYa1lJHLFuKowF3pWF7o",
+  authDomain: "ibring-49f75.firebaseapp.com",
+  databaseURL: "https://ibring-49f75-default-rtdb.firebaseio.com",
+  projectId: "ibring-49f75",
+  storageBucket: "ibring-49f75.appspot.com",
+  messagingSenderId: "344766203491",
+  appId: "1:344766203491:web:cc383e7a60c09f4b24da56",
+  measurementId: "G-MSJQ9Q5VS7"} 
+};
---
+++ ibring/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {apiKey: "AIzaSyD6lgC3EQNLzFtKYa1lJHLFuKowF3pWF7o",
+  authDomain: "ibring-49f75.firebaseapp.com",
+  databaseURL: "https://ibring-49f75-default-rtdb.firebaseio.com",
+  projectId: "ibring-49f75",
+  storageBucket: "ibring-49f75.appspot.com",
+  messagingSenderId: "344766203491",
+  appId: "1:344766203491:web:cc383e7a60c09f4b24da56",
+  measurementId: "G-MSJQ9Q5VS7"} 
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
